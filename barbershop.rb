446f62a03dc2d17404724172345a00a0e8945314
Andrey777-ops <chub-andrey@mail.ru>
2021-10-20 23:25:22 +0300

    Add post

--- barbershop.rb
+++ barbershop.rb
@@ -3,4 +3,14 @@ require 'sinatra'
 get '/' do
   erb :index
 end
-#=======================================
+
+post '/' do
+  @user_name = params[:user_name]
+  @phone     = params[:phone]
+  @data_time = params[:data_time]
+
+  @title = 'Thank you!'
+  @message = "Dear #{@user_name}, we`ll be waiting for you at #{@data_time}"
+
+  erb :message
+end
